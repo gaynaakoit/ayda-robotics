625d6519f9a0c8ad65f5ce33d40aad8f19f8d10c
Abdoulaye kane <abdoulaye.kane093@gmail.com>
2026-01-19 13:56:21 +0000

    Fix bug from backend gateway event emit

--- backend/ayda-backend/src/events/event.gateway.ts
+++ backend/ayda-backend/src/events/event.gateway.ts
@@ -1,52 +1,50 @@
-import {
-    WebSocketGateway,
-    WebSocketServer,
-    OnGatewayInit,
-    OnGatewayConnection,
-    OnGatewayDisconnect
-  } from '@nestjs/websockets';
-  import { Server, Socket } from 'socket.io';
-  import { v4 as uuid } from 'uuid';
-import { FaceDetectedDto } from './dto/face-event.dto';
+  import {
+      WebSocketGateway,
+      WebSocketServer,
+      OnGatewayInit,
+      OnGatewayConnection,
+      OnGatewayDisconnect
+    } from '@nestjs/websockets';
+    import { Server, Socket } from 'socket.io';
+    import { v4 as uuid } from 'uuid';
+  import { FaceDetectedDto } from './dto/face-event.dto';
 
-  @WebSocketGateway({ cors: { origin: '*' } })
-  export class EventsGateway
-    implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-  
-    @WebSocketServer()
-    server!: Server;
-  
-    afterInit() {
-      console.log('✅ Socket.IO initialisé');
+    @WebSocketGateway({ cors: { origin: '*' } })
+    export class EventsGateway
+      implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+    
+      @WebSocketServer()
+      server!: Server;
+    
+      afterInit() {
+        console.log('✅ Socket.IO initialisé');
+      }
+    
+      handleConnection(client: Socket) {
+        console.log('🟢 Client connecté:', client.id);
+      }
+    
+      handleDisconnect(client: Socket) {
+        console.log('🔴 Client déconnecté:', client.id);
+      }
+    
+      emitFaceDetected(faces: FaceDetectedDto) {
+        console.log('📡 gateway active', 'events', {
+          id: uuid(),
+          type: 'FACE_DETECTED',
+          source: 'AI',
+          timestamp: new Date().toISOString(),
+          payload: {
+            faces
+          }
+        });
+        this.server.emit('events', {
+          id: uuid(),
+          type: 'FACE_DETECTED',
+          source: 'AI',
+          timestamp: new Date().toISOString(),
+          payload: faces
+        });
+      }
     }
-  
-    handleConnection(client: Socket) {
-      console.log('🟢 Client connecté:', client.id);
-    }
-  
-    handleDisconnect(client: Socket) {
-      console.log('🔴 Client déconnecté:', client.id);
-    }
-  
-    emitFaceDetected(faces: FaceDetectedDto) {
-      console.log('📡 gateway active', 'events', {
-        id: uuid(),
-        type: 'FACE_DETECTED',
-        source: 'AI',
-        timestamp: new Date().toISOString(),
-        payload: {
-          faces
-        }
-      });
-      this.server.emit('events', {
-        id: uuid(),
-        type: 'FACE_DETECTED',
-        source: 'AI',
-        timestamp: new Date().toISOString(),
-        payload: {
-          faces
-        }
-      });
-    }
-  }
-  
+    
